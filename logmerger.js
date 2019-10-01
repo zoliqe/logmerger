dd23bfd61d8beace04f2a02d2cbb96759ffa8ae2
Mike Karas OM4AA <mike.om4aa@gmail.com>
2019-10-01 20:59:09 +0200

    ignore clearing feature

--- logmerger.js
+++ logmerger.js
@@ -1,24 +1,27 @@
 const fs = require('fs')
 const adif = require('adif-gsp')
 
+const ignore = 'IGNORE'
+const yes = 'Y'
 const qsosMatch = (q1, q2) => q1.call === q2.call && q1.band === q2.band && q1.mode === q2.mode // TODO remove suffixes?
-const qsoIgnored = qso => qso.qsl_sent_via === 'IGNORE' || qso.qsl_sent === 'Y'
+const qsoIgnored = qso => qso.qsl_sent_via === ignore || qso.qsl_sent === yes
 
-const qsos = parseLog('log.adif')
-const contestQsos = parseLog('contest.adif')
+const qsos = parseLog('log.adi')
+const contestQsos = parseLog('contest.adi')
 if (contestQsos) {
 	qsos = [...qsos, ...contestQsos]
 	console.info('Total QSOs count', qsos.length)
 }
 // console.log(qsos[0])
 //	qsos.forEach(qso => console.log(`${qso.call} ${qso.band} ${qso.mode} ${qso.qsl_sent_via}`))
+clearQslIgnoreIfSent(qsos)
 findAndMarkQslIgnore(qsos)
 
 const writer = new adif.AdiWriter("LogMerger", "1.0")
 const output = writer.writeAll(qsos)
-fs.writeFile('log2.adif', output, err => {
+fs.writeFile('log2.adi', output, err => {
 	if (err) log.error('Error writing log file', err)
-	else console.info('Log log2.adif written')
+	else console.info('Log log2.adi written')
 })
 
 
@@ -37,6 +40,15 @@ function parseLog(logname) {
 	}
 }
 
+function clearQslIgnoreIfSent(qsos) {
+	const ignSent = qsos
+		.filter(qso => qso.qsl_sent_via === ignore)
+		.filter(qso => qso.qsl_sent     === yes)
+	ignSent
+		.forEach(qso => delete qso.qsl_sent_via)
+	console.info('QSOs cleared QSL IGNORE:', ignSent.length)
+}
+
 function findAndMarkQslIgnore(qsos) {
 	const ignored = qsos.filter(qsoIgnored)
 	console.info('QSOs with QSL sent or IGNORE:', ignored.length)
@@ -51,9 +63,9 @@ function findAndMarkQslIgnore(qsos) {
 function markQsosAsQslIgnoreFor(ignored, qsos) {
 	qsos
 		.filter(qso => !qso.qsl_sent_via) // not marked as IGNORE
-		.filter(qso => qso.qsl_sent !== 'Y') // QSL not sent
+		.filter(qso => qso.qsl_sent !== yes) // QSL not sent
 		.filter(qso => matchIgnored(qso, ignored))
-		.forEach(qso => qso.qsl_sent_via = 'IGNORE')
+		.forEach(qso => qso.qsl_sent_via = ignore)
 }
 
 function matchIgnored(qso, ignored) {
